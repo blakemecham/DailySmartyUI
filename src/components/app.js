7ef1b73722d2988f86539f572f04bd73bb4dbd8d
blakemecham <bam.mecham@gmail.com>
2018-08-23 15:09:26 -0600

    added img to page

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Flying Rainbow Ninjas!</h1>
-        
+        <div>
+          <Logo/>
+        </div>
       </div>
     );
   }
